e450be1df6e4fb81f65743516c3bc21e58f39d9a
Michael Weinstein <michael.weinstein@ucla.edu>
2018-04-24 22:31:32 -0700

    Better data viewer and init files that do what they're supposed to.

--- runners/variantReaders/dataViewer.py
+++ runners/variantReaders/dataViewer.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python3
 
-fileName = "LB2907_DNA.acceptedSomatics.mPileupRNASupport.pkl"
-if not fileName:
-    raise RuntimeError("File name has to be set")
+def openVariantsPickle(fileName):
+    if not fileName:
+        raise RuntimeError("File name has to be set")
+    import pickle
+    file = open(fileName, 'rb')
+    data = pickle.load(file)
+    file.close()
+    return data
 
-import pickle
-file = open(fileName, 'rb')
-data = pickle.load(file)
-file.close()
-variantsOfInterest = {}
-for variant in data:
-    if data[variant]["RNASupport"].totalDepth:
-        variantsOfInterest[variant] = data[variant]
-print(fileName)
+if __name__ == "__main__":
+    fileName = "LB2907_DNA.acceptedSomatics.mPileupRNASupport.pkl"
+    data = openVariantsPickle(fileName)
+    print("Data headers: %s" %data.keys())
